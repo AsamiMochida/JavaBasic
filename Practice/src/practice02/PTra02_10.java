0377ce4d61d149fae316ef67523a0a98357f6d00
Mochida Asami <ahirutaicho61@gmail.com>
2018-04-23 12:03:16 +0900

    PTra02_10まで完了

--- Practice/src/practice02/PTra02_10.java
+++ Practice/src/practice02/PTra02_10.java
@@ -21,6 +21,10 @@ public class PTra02_10 {
 		 * ※ 日本円には小数点がありませんので、整数の値で出力してください(少数部分は切り捨て)
 		 * ※ プログラムは何行書いても良いです
 		 */
-
+		coffee *=2;
+		
+		int total = (coffee + snack) *108 / 100;
+		moneyInHand -= total;
+		System.out.println(moneyInHand);
 	}
 }
