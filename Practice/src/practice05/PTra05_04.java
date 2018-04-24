597b584e4a31953a2d8af1b0d84aa609f9c4e338
Mochida Asami <ahirutaicho61@gmail.com>
2018-04-24 18:07:09 +0900

    PTra05_04まで完了

--- Practice/src/practice05/PTra05_04.java
+++ Practice/src/practice05/PTra05_04.java
@@ -23,16 +23,45 @@ public class PTra05_04 {
 		 */
 
 		// ①
-		System.out.println("0～6の整数を入力してください");
+		boolean isloop = true;
+		while(isloop) {
+			System.out.println("0～6の整数を入力してください");
 
-		String line = scanner.nextLine();
+			String line = scanner.nextLine();
 
-		switch(line) {
-			case "0":
-				System.out.println("日曜");
-				break;
-			case "1":
-				System.out.println("月曜");
+			switch(line) {
+				case "0":
+					System.out.println("日曜");
+					isloop = false;
+					break;
+				case "1":
+					System.out.println("月曜");
+					isloop = false;
+					break;
+				case "2":
+					System.out.println("火曜");
+					isloop = false;
+					break;
+				case "3":
+					System.out.println("水曜");
+					isloop = false;
+					break;
+				case "4":
+					System.out.println("木曜");
+					isloop = false;
+					break;
+				case "5":
+					System.out.println("金曜");
+					isloop = false;
+					break;
+				case "6":
+					System.out.println("土曜");
+					isloop = false;
+					break;
+				default:
+			}
+
 		}
+		System.out.println("プログラムを終了します。");
 	}
 }
