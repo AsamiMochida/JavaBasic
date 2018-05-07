e1330b66df7bd9ed8342de5c68c50f3cd012d00d
Mochida Asami <ahirutaicho61@gmail.com>
2018-05-07 18:21:53 +0900

    Practice10_05 まで完了

---
+++ Practice/src/practice10/User.java
@@ -0,0 +1,19 @@
+package practice10;
+
+public class User {
+	int userId;
+	String userNm;
+	String mail;
+	String password;
+}
+
+/* クラス名
+* 		User
+* フィールド
+* 		userId		:	int型
+* 		userNm		：	String型
+* 		mail		：	String型
+* 		password	:	String型
+* メソッド
+* 		なし
+*/
