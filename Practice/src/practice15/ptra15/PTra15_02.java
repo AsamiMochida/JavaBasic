e38c2f664dcc1197c40347a399e752b22899e8e1
Mochida Asami <ahirutaicho61@gmail.com>
2018-05-17 10:26:38 +0900

    PTra15-02まで完了

--- Practice/src/practice15/ptra15/PTra15_02.java
+++ Practice/src/practice15/ptra15/PTra15_02.java
@@ -15,9 +15,17 @@ public class PTra15_02 {
 		Room room = new Java2Month();
 
 		// roomに格納されたJava2MonthインスタンスのgetCurriculumメソッドを呼び出してください
+		// メモ：分かっていないといけないポイント！●Room型にgetCurriculumメソッドはない。Java2Monthクラスにあるため、(p4おおまかに捉える際の制約　の所)
+		Java2Month jRoom = (Java2Month)room;
+		// ↑メモ：キャスト演算子を使い、強制的に型変換をする
 
+		jRoom.getCurriculum();
 
 		// 取得したカリキュラム情報を全て出力してください
-
+		String[] j = new String[7];
+		for(int i = 0; i < j.length; i++) {
+			j = jRoom.getCurriculum();
+			System.out.println(j[i]);
+		}
 	}
 }
