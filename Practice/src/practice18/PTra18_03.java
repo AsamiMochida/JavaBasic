56f3d35e4acce2f0cfa02fa9b603d5e868bb9a4e
Mochida Asami <ahirutaicho61@gmail.com>
2018-05-21 10:57:11 +0900

    PTra18_04まで完了

--- Practice/src/practice18/PTra18_03.java
+++ Practice/src/practice18/PTra18_03.java
@@ -6,6 +6,13 @@
  */
 package practice18;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import entity.Player;
+
 public class PTra18_03 {
 
 	/*
@@ -19,13 +26,36 @@ public class PTra18_03 {
 		 * ★ file/BestElevenCandidate.csvの内容を取得し、１行毎にPlayerインスタンスに情報を格納してください
 		 * ★ ArrayListを作成して、Playerインスタンスを格納してください
 		 */
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(new File("file/BestElevenCandidate.csv"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 
-
+		ArrayList<Player> playerList = new ArrayList<>();
+		while (scanner.hasNext()) {
+			Player player = new Player();
+			String line = scanner.nextLine();
+			String[] strlist = line.split(",");
+			player.setPosition(strlist[0]);
+			player.setName(strlist[1]);
+			player.setCountry(strlist[2]);
+			player.setTeam(strlist[3]);
+			playerList.add(player);
+		}
 		// ★ ①のArrayListの中から"レアル・マドリード", "バルセロナ"の選手を除外してください
-
-
+//		メモ：ArrayListの中身の箱をremove(除外)すると、消えた箱の番号も箱ごと消えるので、次に控えていた箱の番号が消えた番号がを引き継いでしまう。(なくなった分詰められてしまうから数が変わる)
+//		      ↓だからArrayListの後ろから消したいワードを探していくとよし
+		for(int i = playerList.size() -1;i >= 0; i--) {
+			if(playerList.get(i).getTeam().equals("レアル・マドリード") || playerList.get(i).getTeam().equals("バルセロナ")) {
+				playerList.remove(i);
+			}
+		}
 		// ★ 削除後のArrayListの中身を全件出力してください
-
+		for (Player str : playerList) {
+			System.out.println(str.toString());
+		}
 
 	}
 }
--- Practice/src/practice18/PTra18_04.java
+++ Practice/src/practice18/PTra18_04.java
@@ -6,6 +6,14 @@
  */
 package practice18;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+import entity.Player;
+
 public class PTra18_04 {
 
 	/*
@@ -19,9 +27,60 @@ public class PTra18_04 {
 		 * ★ file/BestElevenCandidate.csvの内容を取得し、１行毎にPlayerインスタンスに情報を格納してください
 		 * ★ ArrayListを作成して、Playerインスタンスを格納してください
 		 */
-
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(new File("file/BestElevenCandidate.csv"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		ArrayList<Player> playerList = new ArrayList<>();
+		while (scanner.hasNext()) {
+			Player player = new Player();
+			String line = scanner.nextLine();
+			String[] strlist = line.split(",");
+			player.setPosition(strlist[0]);
+			player.setName(strlist[1]);
+			player.setCountry(strlist[2]);
+			player.setTeam(strlist[3]);
+			playerList.add(player);
+		}
 
 		// ★ ①のArrayListの中からGK1名、DF4名、MF4名, FW2名をランダムで出力してください
+		Collections.shuffle(playerList);
 
+		int gkCount = 0;
+		int dfCount = 0;
+		int mfCount = 0;
+		int fwCount = 0;
+
+		for (int i = 0; i < playerList.size(); i++) {
+
+			if(playerList.get(i).getPosition().equals("GK")) {
+				if(gkCount < 1) {
+					System.out.println(playerList.get(i));
+					gkCount += 1;
+				}
+			}
+
+			if(playerList.get(i).getPosition().equals("DF")) {
+				if(dfCount < 4) {
+					System.out.println(playerList.get(i));
+					dfCount += 1;
+				}
+			}
+			if(playerList.get(i).getPosition().equals("MF")) {
+				if(mfCount < 4) {
+					System.out.println(playerList.get(i));
+					mfCount += 1;
+				}
+			}
+			if(playerList.get(i).getPosition().equals("FW")) {
+				if(fwCount < 2) {
+					System.out.println(playerList.get(i));
+					fwCount += 1;
+				}
+			}
+		}
+		// 作ったカウントを、そのポジションの選手を表示したタイミングで増やす。表示するときにカウントが○ヨリチイサイバアイミタイカンジデツクッテネ
 	}
 }
